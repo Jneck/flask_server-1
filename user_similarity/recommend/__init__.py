bc54e7bcadee704e31bf48bac572ddf28f7c0b82
naya-beene <lsb9490@naver.com>
2022-09-01 22:07:33 +0900

    user_similarity_final_commit

--- user_similarity/recommend/__init__.py
+++ user_similarity/recommend/__init__.py
@@ -1,25 +1,13 @@
-from flask import Flask
-from flask import render_template
-
-#pip install flask-migrate
-#pip install pymysql
-#flask db init
-#flask run --port=5001
-#flask db migrate
-#flask db upgrade
-
-
+from flask import Flask,request, jsonify
 
 def create_app():
     app = Flask(__name__)
 
     from .models import user_similarity
 
-    @app.route('/')
+    @app.route('/similarity', methods=['POST'])
     def index():
-
-        return user_similarity()
-
+        return jsonify({'similarity': user_similarity(dict(request.json))})
     return app
 
 #터미널 : set FALSK_APP=패키지 이름,  set FLASK_DEBUG=true
--- user_similarity/recommend/models.py
+++ user_similarity/recommend/models.py
@@ -7,9 +7,20 @@ from sklearn.metrics import mean_squared_error
 from scipy.sparse import csr_matrix
 import json
 from sklearn.metrics.pairwise import cosine_similarity
+import csv
+import json
 
-def user_similarity():
-    click_df = pd.read_csv('./recommend/data/clicks.csv', encoding='utf-8')
+def user_similarity(data):
+
+    dic_list = data['Results']
+
+    member_id = str(data['member_id'])
+
+    d_list = []
+    for row in dic_list:
+        d_list.append([row['member_id'], row['feed_id'], row['click_count']])
+
+    click_df = pd.DataFrame(d_list, columns=['member_id', 'feed_id', 'click_count'])
 
     title_user = click_df.pivot_table('click_count', index='member_id',columns='feed_id')
     title_user.fillna(0, inplace=True)
@@ -17,7 +28,15 @@ def user_similarity():
     # 유저와 유저 간의 유사도
     user_based_collab = cosine_similarity(title_user, title_user)
     user_based_collab = pd.DataFrame(user_based_collab, index=title_user.index, columns=title_user.index)
-    tmplist = user_based_collab[1].sort_values(ascending=False)[1:11].index
+
+
+    tmplist=[]
+    if 11 > len(user_based_collab.index):
+        tmplist = user_based_collab[member_id].sort_values(ascending=False)[1:].index
+
+    else:
+        tmplist = user_based_collab[member_id].sort_values(ascending=False)[1:11].index
+
     similarlist = []
     for s in tmplist:
         similarlist.append(str(s))
